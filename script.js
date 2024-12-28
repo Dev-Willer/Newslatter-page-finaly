b8877120d4efa26308688ed3978192e63640e442
Dev-Willer <josewiller2018@gmail.com>
2024-12-28 03:18:01 -0300

    add scripts in site

---
+++ script.js
@@ -0,0 +1,46 @@
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal('.efeito-h2-aside', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+})
+
+revelar.reveal('.efeito-h2-aside1',{
+    duration: 4000,
+    origin: 'right',
+    distance: '200px' 
+})
+
+revelar.reveal('.efeito-p-aside',{
+    duration: 5000,
+    origin: 'left',
+    distance: '50px' 
+})
+
+//FORMULARIO
+
+revelar.reveal('.efeito-form1',{
+    duration: 2000,
+    origin: 'left',
+    distance: '200px' 
+})
+
+revelar.reveal('.efeito-form2',{
+    duration: 3000,
+    origin: 'right',
+    distance: '200px' 
+})
+
+revelar.reveal('.efeito', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px' 
+})
+
+revelar.reveal('.efeito-img', {
+    duration: 5000,
+    origin: 'right',
+    distance: '100px',
+    delay: 1000
+})
